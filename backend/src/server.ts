cab69a8a67150ce11cbd2dd183f556441a97617d
Itsuki06 <trinhtung0604@gmail.com>
2026-01-22 23:07:30 +0700

    feat: config env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
-import express from "express";
 import app from "./app";
+import { connectDB } from "./config/db";
+import { env } from "./config/env";
 
-const PORT = 5000;
+const PORT = env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
