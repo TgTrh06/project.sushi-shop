8dd7e59049cbd37fa55a59661d029d89e81766d2
Itsuki06 <trinhtung0604@gmail.com>
2026-03-03 09:49:20 +0700

    refactor router files

---
+++ src/routes/api/index.ts
@@ -0,0 +1,2 @@
+// Convention file for aggregating all API routes in one place
+import express from "express";
--- src/routes/api/product.routes.ts
+++ src/routes/api/product.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as controller from "../controllers/product.controller";
+import * as controller from "../../controllers/product.controller";
 
 const router = Router();
 
