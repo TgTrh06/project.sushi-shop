740eefdfb9ced8a1c0eee99f948b367179084747
Itsuki06 <shyorn0604@gmail.com>
2026-03-05 11:08:48 +0700

    feat: category type fix

--- backend/src/services/category.service.ts
+++ backend/src/services/category.service.ts
@@ -1,12 +1,12 @@
 import { CategoryModel } from "../models/category/category.model";
-import { ICategory } from "../models/category/category.types";
+import { Category } from "../models/category/category.types";
 
 export const createCategory = async (
-  data: Partial<ICategory>,
-): Promise<ICategory> => {
+  data: Partial<Category>,
+): Promise<Category> => {
   return await CategoryModel.create(data);
 };
 
-export const getAllCategories = async (): Promise<ICategory[]> => {
+export const getAllCategories = async (): Promise<Category[]> => {
   return await CategoryModel.find();
 };
