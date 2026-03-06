83a82a0955ea741a955d18b9fcc6dbdf184a4013
Itsuki06 <shyorn0604@gmail.com>
2026-03-06 11:42:21 +0700

    feat: auth controller

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,27 @@
+import { AuthService } from "../services/auth.service";
+import { Request, Response, NextFunction } from "express";
+import { ResponseHandler } from "../utils/response.utils";
+import { LoginDTO } from "../models/user/user.types";
+
+const authService = new AuthService();
+
+export class AuthController {
+  static async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await authService.register(req.body);
+      return ResponseHandler.created(res, user, "User registered successfully.");
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const loginDto: LoginDTO = req.body;
+      const token = await authService.login(loginDto);
+      return ResponseHandler.success(res, { token }, "Login successful");
+    } catch (error) {
+      next(error);
+    }
+  }
+}
