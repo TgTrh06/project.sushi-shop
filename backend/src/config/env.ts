3e5b29b2312ec8cd2fbc678ca819d4f6af8875e0
Itsuki06 <trinhtung0604@gmail.com>
2026-03-06 11:11:41 +0700

    doc: add swagger, winston, passport.js, jwt

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,4 +4,6 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI as string,
+  JWT_SECRET: process.env.JWT_SECRET as string,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string,
 };
