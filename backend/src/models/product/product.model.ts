a48cbc276ee00a73358165e22aeb22314e9a1a4d
Itsuki06 <shyorn0604@gmail.com>
2026-03-09 23:58:04 +0700

    feat: product model

--- backend/src/models/product/product.model.ts
+++ backend/src/models/product/product.model.ts
@@ -1,25 +1,15 @@
-import { Schema, model, Document, Types } from "mongoose";
+import { Schema, model, Types } from "mongoose";
+import { ProductDocument } from "./product.types";
 
-export interface IProduct extends Document {
-  name: string;
-  description?: string;
-  price: number;
-  imageUrl?: string;
-  category: Types.ObjectId;
-  isAvailable: boolean;
-  stockQuantity: number;
-  createdAt: Date;
-}
-
-const ProductSchema = new Schema<IProduct>({
+const ProductSchema = new Schema<ProductDocument>({
   name: { type: String, required: true },
   description: { type: String },
   price: { type: Number, required: true },
   imageUrl: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  category: { type: Types.ObjectId, ref: "Category", required: true },
   isAvailable: { type: Boolean, default: true },
   stockQuantity: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
 });
 
-export const ProductModel = model<IProduct>("Product", ProductSchema);
+export const ProductModel = model<ProductDocument>("Product", ProductSchema);
