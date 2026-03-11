b1149931f73a3f7022562592ac18491a29197dff
Itsuki06 <shyorn0604@gmail.com>
2026-03-11 09:28:48 +0700

    feat: env config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const env = {
   PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI as string,
-  JWT_SECRET: process.env.JWT_SECRET as string,
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 };
