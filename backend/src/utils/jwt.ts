5f0dda17e230cba0cf443c56f1b08f767bc86509
Itsuki06 <trinhtung0604@gmail.com>
2026-03-11 09:27:40 +0700

    doc: jwt utils

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,16 +1,23 @@
-import jwt from "jsonwebtoken";
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
 import { UserEntity } from "../models/user/user.types";
+import { Response } from "express";
+import { env } from "../config/env";
 
-export const generateToken = (user: UserEntity) => {
-  return jwt.sign(
-    {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-    },
-    process.env.JWT_SECRET as string,
-    { 
-      expiresIn: "1h" 
-    },
-  );
+export const generateToken = (user: UserEntity): string => {
+  const payload = { user };
+  const secret: Secret = env.JWT_SECRET as string;
+  const options: SignOptions = {
+    expiresIn: env.JWT_EXPIRES_IN as any,
+  }
+
+  return jwt.sign(payload, secret, options);
+};
+
+export const setCookies = (res: Response, token: string): void => {
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  });
 };
