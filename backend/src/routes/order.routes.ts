9d6f09cc3d3b5606cd8b3248a4a29b32816ec809
Itsuki06 <trinhtung0604@gmail.com>
2026-03-12 14:05:03 +0700

    feat: fix routes

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { OrderController } from "../controllers/order.controller";
+import OrderController from "../controllers/order.controller";
 
 const router = Router();
 
