17bac36dcc837855874751ef4481b1c7b258986c
Itsuki06 <trinhtung0604@gmail.com>
2026-03-14 23:43:00 +0700

    feat: auth & user schemas

--- frontend/src/schemas/auth.schema.ts
+++ frontend/src/schemas/auth.schema.ts
@@ -1,8 +1,8 @@
-import { z } from "zod";
+import z from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email("Invalid email"),
-  password: z.string().min(6, "Password must have least 6 characters"),
+  password: z.string().min(6, "Password must be least 6 characters"),
 });
 
-export type LoginInput = z.infer<typeof LoginSchema>;
+export type LoginDTO = z.infer<typeof LoginSchema>;
