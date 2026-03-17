c42d8647284fd614d754b3080ae48ff2dcdcaa08
Itsuki06 <trinhtung0604@gmail.com>
2026-03-17 11:29:36 +0700

    feat: /me route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import UserController from "../controllers/user.controller";
+import { verifyToken } from "../middleware/auth.middleware";
 
 const router = Router();
+
+// Route to get profile of the current logged-in user
+router.get("/me", verifyToken, UserController.getProfile);
 
 router.get("/", UserController.getAllUsers);
 router.get("/:id", UserController.getUserById);
