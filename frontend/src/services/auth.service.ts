f1978309c03d22c3ecf1ec46ddea130c1efd3520
Itsuki06 <trinhtung0604@gmail.com>
2026-03-18 23:32:35 +0700

    feat: auth service

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,9 +1,19 @@
 import api from "../lib/api";
-import type { LoginInput } from "../schemas/auth.schema";
+import type { LoginInput, RegisterInput } from "../schemas/auth.schema";
+import { type ApiResponse } from "../types/api.type";
+
+export interface AuthResponse {
+  token: string;
+}
 
 export const authService = {
-  login: async (input: LoginInput) => {
-    const res = await api.post("/auth/login", input)
-    return res.data.data
+  login: async (input: LoginInput): Promise<AuthResponse> => {
+    const result = await api.post<ApiResponse<AuthResponse>>("/auth/login", input)
+    return result.data.data
+  },
+
+  register: async (input: RegisterInput) => {
+    const result = await api.post<ApiResponse<AuthResponse>>("/auth/register", input)
+    return result.data.data
   }
 }
