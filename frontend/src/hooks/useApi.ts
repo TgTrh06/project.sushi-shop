d4f0d0dfeb21dc1ec31a6c04f946ceed7cf7184d
Itsuki06 <trinhtung0604@gmail.com>
2026-03-19 16:30:19 +0700

    feat: error handler

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
 export const useApi = () => {
-  const [globalError, setGlobalError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
-  return { globalError, setGlobalError, loading, setLoading };
+  return { loading, setLoading };
 };
