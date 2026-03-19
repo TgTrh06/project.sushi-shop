7566ca157288376dfb1a9a1899069f047656bd60
Itsuki06 <shyorn0604@gmail.com>
2026-03-19 14:50:30 +0700

    feat: change to user cookie instead of local storage

--- frontend/src/features/auth/auth.service.ts
+++ frontend/src/features/auth/auth.service.ts
@@ -1,19 +1,30 @@
 import api from "../../lib/api";
 import type { LoginInput, RegisterInput } from "./auth.schema";
 import { type ApiResponse } from "../../types/response.type";
+import type { User } from "./auth.types";
 
 export interface AuthResponse {
-  token: string;
+  user: User;
 }
 
 export const authService = {
   login: async (input: LoginInput): Promise<AuthResponse> => {
-    const result = await api.post<ApiResponse<AuthResponse>>("/auth/login", input)
-    return result.data.data
+    const result = await api.post<ApiResponse<AuthResponse>>(
+      "/auth/login",
+      input,
+    );
+    return result.data.data;
   },
 
-  register: async (input: RegisterInput) => {
-    const result = await api.post<ApiResponse<AuthResponse>>("/auth/register", input)
-    return result.data.data
-  }
-}
+  register: async (input: RegisterInput): Promise<AuthResponse> => {
+    const result = await api.post<ApiResponse<AuthResponse>>(
+      "/auth/register",
+      input,
+    );
+    return result.data.data;
+  },
+
+  logout: async () => {
+    await api.post("/auth/logout");
+  },
+};
