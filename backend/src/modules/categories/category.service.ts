4d3b44761f7c9aa142b7f7326026101c08b70dc2
Itsuki06 <shyorn0604@gmail.com>
2026-03-21 23:30:50 +0700

    feat: change CatService to OOP concept

--- backend/src/modules/categories/category.service.ts
+++ backend/src/modules/categories/category.service.ts
@@ -1,12 +1,20 @@
-import { CategoryModel } from "./category.model";
-import { CategoryEntity } from "./category.types";
+import { ConflictError } from "../../core/utils/common/error.utils";
+import CategoryRepository from "./category.repository";
+import { CategoryEntity, CreateCategoryDTO } from "./category.types";
 
-export const createCategory = async (
-  data: Partial<CategoryEntity>,
-): Promise<CategoryEntity> => {
-  return await CategoryModel.create(data);
-};
+export default class CategoryService {
+  private repo = new CategoryRepository();
 
-export const getAllCategories = async (): Promise<CategoryEntity[]> => {
-  return await CategoryModel.find();
-};
+  async getAllCategories(): Promise<CategoryEntity[]> {
+    return await this.repo.findAll();
+  }
+
+  async createCategory(dto: CreateCategoryDTO): Promise<CategoryEntity> {
+    const existingCategory = await this.repo.findByName(dto.name)
+    if (existingCategory) {
+      throw new ConflictError("Category already exists.");
+    }
+
+    return await this.repo.create(dto);
+  }
+}
