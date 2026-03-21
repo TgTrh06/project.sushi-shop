93fc4838e6b220428dc4e7819afdaf706b31d55a
Itsuki06 <shyorn0604@gmail.com>
2026-03-21 23:33:51 +0700

    feat: change CatController to OOP concept

--- backend/src/modules/categories/category.controller.ts
+++ backend/src/modules/categories/category.controller.ts
@@ -4,28 +4,30 @@ import { ResponseHandler } from "../../core/utils/common/response.utils";
 
 const categoryService = new CategoryService();
 
-export const createCategory = async (
-  req: Request, 
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const newCategory = await categoryService.createCategory(req.body);
-    return ResponseHandler.created(res, newCategory, "Category created successfully.");
-  } catch (error) {
-    next(error);
+export default class CategoryController {
+  static async createCategory(req: Request, res: Response, next: NextFunction) {
+    try {
+      const newCategory = await categoryService.createCategory(req.body);
+      return ResponseHandler.created(
+        res,
+        newCategory,
+        "Category created successfully.",
+      );
+    } catch (error) {
+      next(error);
+    }
   }
-};
 
-export const getAllCategories = async (
-  _req: Request, 
-  res: Response, 
-  next: NextFunction
-) => {
-  try {
-    const categories = await categoryService.getAllCategories();
-    return ResponseHandler.success(res, categories, "Categories retrieved successfully.");
-  } catch (error) {
-    next(error);
+  static async getAllCategories(_req: Request, res: Response, next: NextFunction) {
+    try {
+      const categories = await categoryService.getAllCategories();
+      return ResponseHandler.success(
+        res,
+        categories,
+        "Categories retrieved successfully.",
+      );
+    } catch (error) {
+      next(error);
+    }
   }
-};
+}
