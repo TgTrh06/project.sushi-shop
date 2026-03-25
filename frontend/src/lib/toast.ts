8a1cc276d14b2c853160b30d856d4de9b128a4fc
Itsuki06 <trinhtung0604@gmail.com>
2026-03-25 20:50:48 +0700

    feat: change noti

--- frontend/src/lib/toast.ts
+++ frontend/src/lib/toast.ts
@@ -1,9 +1,11 @@
 import toast from "react-hot-toast";
 
-export const showSuccess = (message: string) => {
-  toast.success(message);
-};
+export default class ToastNoti {
+  showSuccess = (message: string) => {
+    toast.success(message);
+  };
 
-export const showError = (message: string) => {
-  toast.error(message);
+  showError = (message: string) => {
+    toast.error(message);
+  };
 };
