19f406ef94cdd72ef7d993e40819fa050469fc75
Itsuki06 <shyorn0604@gmail.com>
2026-03-26 14:00:13 +0700

    feat: toast noti

--- frontend/src/lib/toast.ts
+++ frontend/src/lib/toast.ts
@@ -1,11 +1,9 @@
 import toast from "react-hot-toast";
 
-export default class ToastNoti {
-  showSuccess = (message: string) => {
-    toast.success(message);
-  };
+export const showSuccess = (message: string) => {
+  toast.success(message);
+};
 
-  showError = (message: string) => {
-    toast.error(message);
-  };
+export const showError = (message: string) => {
+  toast.error(message);
 };
