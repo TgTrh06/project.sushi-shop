66f0cafe3000759e7913779c0c8f2eb67c950ddf
Itsuki06 <shyorn0604@gmail.com>
2026-03-30 15:06:02 +0700

    feat: Error utils

--- backend/src/core/utils/common/error.utils.ts
+++ backend/src/core/utils/common/error.utils.ts
@@ -1,12 +1,12 @@
 export class AppError extends Error {
-  public statusCode: number;
-  public isOperational: boolean;
+  public readonly statusCode: number;
+  public readonly isOperational: boolean;
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: number, isOperational = true) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = true;
-
+    this.isOperational = isOperational;
+    Object.setPrototypeOf(this, new.target.prototype);
     Error.captureStackTrace(this, this.constructor);
   }
 }
