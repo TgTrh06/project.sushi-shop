44de04ef45455398db8dc15ef42d1db2d7ff46d3
Itsuki06 <trinhtung0604@gmail.com>
2026-03-31 23:19:59 +0700

    feat: navbar changes

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -6,14 +6,15 @@ export const Navbar = () => {
     <header>
       <nav className="header__nav">
         <div className="header__logo">
-          <h4>SushiYa</h4>
+          <h4>ItsuSushi</h4>
           <div className="header__logo-overlay"></div>
         </div>
 
         <ul className="header__menu">
           {/* Dùng thẻ <Link> của React thay cho thẻ <a> để chuyển trang không bị load lại web */}
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Menu</Link></li>
+          <li><Link to="/Menu">Menu</Link></li>
+          <li><Link to="/booking">Booking</Link></li>
           <li><Link to="/about">About Us</Link></li>
           <li><Link to="/login">Login</Link></li>
         </ul>
