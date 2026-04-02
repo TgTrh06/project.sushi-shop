9be4114f96c136a9e26baa507d91b53d912bac16
Itsuki06 <shyorn0604@gmail.com>
2026-04-02 16:53:17 +0700

    feat: refactor

--- apps/frontend/src/features/users/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const updatedProfileSchema = z.object({
-  username: z.string().min(2),
-  // phone: z.string().optional(),
-  // address: z.string().optional(),
-})
-
-export type UpdateProfileDTO = z.infer<typeof updatedProfileSchema>
