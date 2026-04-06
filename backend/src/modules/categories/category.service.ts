8aeae826bffed6a725b221ab4a636dffbed89706
Itsuki06 <shyorn0604@gmail.com>
2026-04-06 23:51:10 +0700

    feat: category service

--- backend/src/modules/categories/category.service.ts
+++ backend/src/modules/categories/category.service.ts
@@ -1,16 +1,23 @@
+import { PaginationResult, PaginationUtils } from "@/utils/common/pagination.utils";
 import { ConflictError } from "../../utils/common/error.utils";
 import CategoryRepository from "./category.repository";
-import { CategoryEntity, CreateCategoryInput } from "./category.types";
+import { CategoryEntity, CreateCategoryFormValues } from "./category.types";
 
 export default class CategoryService {
   private repo = new CategoryRepository();
 
-  async getAllCategories(): Promise<CategoryEntity[]> {
-    return await this.repo.findAll();
+  async getAllCategories(
+    page: number,
+    limit: number,
+    offset: number,
+  ): Promise<PaginationResult<CategoryEntity>> {
+    const { docs, total } = await this.repo.findPaginated(limit, offset);
+    
+    return PaginationUtils.format(docs, total, page, limit);
   }
 
-  async createCategory(dto: CreateCategoryInput): Promise<CategoryEntity> {
-    const existingCategory = await this.repo.findByName(dto.name)
+  async createCategory(dto: CreateCategoryFormValues): Promise<CategoryEntity> {
+    const existingCategory = await this.repo.exists(dto.name);
     if (existingCategory) {
       throw new ConflictError("Category already exists.");
     }
