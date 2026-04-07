284baf0afdec6052ed10a89981e238166d2c328a
Itsuki06 <trinhtung0604@gmail.com>
2026-04-07 23:00:56 +0700

    feat: huge changes category routes

--- backend/src/modules/categories/category.routes.ts
+++ backend/src/modules/categories/category.routes.ts
@@ -1,15 +1,34 @@
 import { Router } from "express";
 import CategoryController from "./category.controller";
 import { verifyAuth, verifyAdmin } from "@/middleware/auth.middleware";
+import { zodValidator } from "@/middleware/validate.middleware";
+import { CreateCategorySchema, UpdateCategorySchema } from "./category.types";
 
 const router = Router();
+const categoryController = new CategoryController();
+
+router.get("/", categoryController.getAll);
+router.get("/:slug", categoryController.getOneBySlug);
 
 router.post(
-  "/", 
-  verifyAuth, 
-  verifyAdmin, 
-  CategoryController.createCategory
+  "/",
+  verifyAuth,
+  verifyAdmin,
+  zodValidator(CreateCategorySchema),
+  categoryController.create,
 );
-router.get("/", CategoryController.getAllCategories);
+router.put(
+  "/:id",
+  verifyAuth,
+  verifyAdmin,
+  zodValidator(UpdateCategorySchema), 
+  categoryController.update
+);
+router.delete(
+  "/:id",
+  verifyAuth,
+  verifyAdmin, 
+  categoryController.delete
+);
 
 export default router;
