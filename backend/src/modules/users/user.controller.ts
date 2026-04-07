056c58c7ef1b1722f2e862369f061f3c3f978c20
Itsuki06 <shyorn0604@gmail.com>
2026-04-07 17:06:48 +0700

    feat: change way of inject user service

--- backend/src/modules/users/user.controller.ts
+++ backend/src/modules/users/user.controller.ts
@@ -3,74 +3,92 @@ import { ResponseHandler } from "../../utils/common/response.utils";
 import UserService from "./user.service";
 import { PaginationUtils } from "../../utils/common/pagination.utils";
 
-const userService = new UserService();
-
 export default class UserController {
+  private static readonly userService = new UserService();
 
   // =========================================================
-  // API CUSTOMER (For USER to manage) 
+  // API CUSTOMER (For USER to manage)
   // NEED to be VERIFIED through "verifyToken"
   // =========================================================
-  
-  // GET /users/me
-  static async getProfile(req: Request, res: Response, next: NextFunction) {
-    try {
-      const safeUser = await userService.getUserById(req.user!.id);
 
-      return ResponseHandler.success(res, safeUser, "Profile retrieved successfully.");
+  // GET /users/me
+  static async getMe(req: Request, res: Response, next: NextFunction) {
+    try {
+      const safeUser = await this.userService.getUserById(req.user!.id);
+
+      return ResponseHandler.success(
+        res,
+        safeUser,
+        "Profile retrieved successfully.",
+      );
     } catch (error) {
       next(error);
     }
   }
 
   // PUT /users/me
-  static async updateProfile(req: Request, res: Response, next: NextFunction) {
+  static async update(req: Request, res: Response, next: NextFunction) {
     try {
-      const newProfile = await userService.updateProfile(req.user!.id, req.body);
+      const newProfile = await this.userService.updateProfile(
+        req.user!.id,
+        req.body,
+      );
 
-      return ResponseHandler.success(res, newProfile, "User updated successfully.")
+      return ResponseHandler.success(
+        res,
+        newProfile,
+        "User updated successfully.",
+      );
     } catch (error) {
       next(error);
     }
-  }  
+  }
 
   // =========================================================
-  // API ADMIN (For ADMIN to manage) 
+  // API ADMIN (For ADMIN to manage)
   // NEED to be VERIFIED through "verifyToken" & "verifyAdmin"
   // =========================================================
 
   // GET /admin/users
-  static async getAllUsers(req: Request, res: Response, next: NextFunction) {
+  static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
       const { page, limit, offset } = PaginationUtils.extract(req.query);
 
-      const paginatedUsers = await userService.getAllUsers(page, limit, offset);
-      
-      return ResponseHandler.success(res, paginatedUsers, "Users retrieved successfully.")
+      const paginatedUsers = await this.userService.getAllUsers(page, limit, offset);
+
+      return ResponseHandler.success(
+        res,
+        paginatedUsers,
+        "Users retrieved successfully.",
+      );
     } catch (error) {
       next(error);
     }
   }
 
   // GET /admin/users/:id
-  static async getUserById(req: Request, res: Response, next: NextFunction) {
+  static async getOneById(req: Request, res: Response, next: NextFunction) {
     try {
-      const safeUser = await userService.getUserById(String(req.params.id));
+      const safeUser = await this.userService.getUserById(String(req.params.id));
 
-      return ResponseHandler.success(res, safeUser, "User retrieved successfully.")
+      return ResponseHandler.success(
+        res,
+        safeUser,
+        "User retrieved successfully.",
+      );
     } catch (error) {
       next(error);
     }
   }
 
   // DELETE /admin/users/:id
-  static async deleteUser(req: Request, res: Response, next: NextFunction) {
+  static async delete(req: Request, res: Response, next: NextFunction) {
     try {
       const targetId = String(req.params.id);
-      const currentAdminId = req.user!.id
+      const currentAdminId = req.user!.id;
 
-      await userService.deleteUser(targetId, currentAdminId);
-      return ResponseHandler.success(res, null, "User deleted successfully.")
+      await this.userService.deleteUser(targetId, currentAdminId);
+      return ResponseHandler.success(res, null, "User deleted successfully.");
     } catch (error) {
       next(error);
     }
