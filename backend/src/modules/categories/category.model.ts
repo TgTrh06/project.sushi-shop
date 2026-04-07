41e3799f818f1a19ff65e6d9b0bbd5f179e83c98
Itsuki06 <shyorn0604@gmail.com>
2026-04-07 17:07:30 +0700

    feat: category types

--- backend/src/modules/categories/category.model.ts
+++ backend/src/modules/categories/category.model.ts
@@ -3,8 +3,10 @@ import { CategoryDocument } from "./category.types";
 
 const CategorySchema = new Schema<CategoryDocument>({
   name: { type: String, required: true },
+  slug: { type: String, required: true},
   description: { type: String },
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 export const CategoryModel = model<CategoryDocument>("Category", CategorySchema);
