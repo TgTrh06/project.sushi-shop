d9af6327cbb1ed8d6bf3c67bdf6cf86681650ce3
Itsuki06 <trinhtung0604@gmail.com>
2026-04-07 17:07:30 +0700

    feat: category types

--- backend/src/modules/categories/category.types.ts
+++ backend/src/modules/categories/category.types.ts
@@ -1,26 +1,37 @@
 import { z } from "zod";
 
-// Business Entity
-export interface CategoryEntity {
-  id: string;
-  name: string;
-  description?: string;
-  createdAt: Date;
-}
+// Base Schema
+export const CategorySchema = z.object({
+  id: z.string(),
+  name: z.string().min(2).max(100),
+  slug: z.string().min(2).max(100).regex(/^[a-z0-0-]+$/),
+  description: z.string().max(250).optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+// Entity shape - Use this for service layer and business logic
+export type CategoryEntity = z.infer<typeof CategorySchema>;
 
 // Database shape - Only use for Mongoose schema definition and database operations
-export interface CategoryDocument extends Omit<CategoryEntity, "id"> {}
+export type CategoryDocument = Omit<CategoryEntity, "id">;
+
+// DTO's Schemas
+export const CreateCategorySchema = CategorySchema.pick({
+  name: true,
+  description: true,
+});
+
+export const UpdateCategorySchema = CreateCategorySchema.partial();
+
+export const GetBySlugSchema = z.object({
+  slug: z.string().min(1, "Slug cannot be empty.")
+});
+
+export const GetByIdSchema = z.object({
+  id: z.string()
+});
 
 // DTOs
-export const CreateCategoryDTO = z.object({
-  name: z.string().min(2).max(100),
-  description: z.string().max(250).optional()
-});
-
-export const UpdateCategoryDTO = z.object({
-  name: z.string().min(2).max(100).optional(),
-  description: z.string().max(250).optional()
-});
-
-export type CreateCategoryFormValues = z.infer<typeof CreateCategoryDTO>;
-export type UpdateCategoryFormValues = z.infer<typeof UpdateCategoryDTO>;
+export type CreateCategoryDTO = z.infer<typeof CreateCategorySchema>;
+export type UpdateCategoryDTO = z.infer<typeof UpdateCategorySchema>;
