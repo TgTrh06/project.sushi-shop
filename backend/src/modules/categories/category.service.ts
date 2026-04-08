b9c1c14bc7c4cf632c1b037bbf0a197c14ce5ef1
Itsuki06 <shyorn0604@gmail.com>
2026-04-08 14:33:37 +0700

    feat: change to use container format with class constructor

--- backend/src/modules/categories/category.service.ts
+++ backend/src/modules/categories/category.service.ts
@@ -2,20 +2,24 @@ import {
   PaginationResult,
   PaginationUtils,
 } from "@/utils/common/pagination.utils";
-import { BadRequestError, ConflictError, NotFoundError } from "@/utils/common/error.utils";
+import {
+  BadRequestError,
+  ConflictError,
+  NotFoundError,
+} from "@/utils/common/error.utils";
 import CategoryRepository from "./category.repository";
 import { CategoryEntity, CreateCategoryDTO } from "./category.types";
 import { generateSlug } from "@/utils/common/slugify.utils";
 
 export default class CategoryService {
-  private repo = new CategoryRepository();
+  constructor(private readonly repo: CategoryRepository) {}
 
   private async checkExist(name: string): Promise<void> {
     const result = await this.repo.existsByName(name);
     if (result) {
       throw new ConflictError("Category with this name already exists.");
     }
-  };
+  }
 
   async getAllCategories(
     page: number,
@@ -25,15 +29,15 @@ export default class CategoryService {
     const { docs, total } = await this.repo.findPaginated(limit, offset);
 
     return PaginationUtils.format(docs, total, page, limit);
-  };
+  }
 
   async getOneBySlug(slug: string): Promise<CategoryEntity | null> {
     if (!slug) {
       throw new BadRequestError("Slug is requied.");
     }
-    
+
     const result = await this.repo.findBySlug(slug);
-    
+
     if (!result) {
       throw new NotFoundError(`Category with slug '${slug}' not found.`);
     }
@@ -45,12 +49,12 @@ export default class CategoryService {
     await this.checkExist(dto.name);
 
     const slug = generateSlug(dto.name);
-    const newData = ({ ...dto, slug });
+    const newData = { ...dto, slug };
 
     const newCategory = await this.repo.create(newData);
 
     return newCategory;
-  };
+  }
 
   async updateCategory(
     id: string,
@@ -73,7 +77,7 @@ export default class CategoryService {
     }
 
     return updatedCategory;
-  };
+  }
 
   async deleteCategory(id: string): Promise<CategoryEntity> {
     const deletedCategory = await this.repo.delete(id);
@@ -82,5 +86,5 @@ export default class CategoryService {
     }
 
     return deletedCategory;
-  };
+  }
 }
