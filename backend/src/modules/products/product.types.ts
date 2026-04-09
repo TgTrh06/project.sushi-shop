c084165ed055b545b759cdf7274ae347d3eaf333
Itsuki06 <shyorn0604@gmail.com>
2026-04-09 23:31:28 +0700

    feat: Product backend types

--- backend/src/modules/products/product.types.ts
+++ backend/src/modules/products/product.types.ts
@@ -1,40 +1,32 @@
-import { Types } from "mongoose";
+import { ProductSchema } from "@shared/schemas/product.schema";
 import { z } from "zod";
 
-// Business Entity
-export interface ProductEntity {
-  id: string;
-  name: string;
-  description?: string;
-  price: number;
-  imageUrl?: string;
-  categoryId: Types.ObjectId;
-  isAvailable: boolean;
-  stockQuantity: number;
-  createdAt: Date;
-}
-
-// Database shape (Mongoose Document)
-export interface ProductDocument extends Omit<ProductEntity, "id"> {}
-
-// DTOs
-export const CreateProductSchema = z.object({
-  name: z.string().min(2).max(100),
-  price: z.number().min(0),
-  description: z.string().max(250).optional(),
-  categoryId: z.string().uuid(),
-  isAvailable: z.boolean().optional(),
-  stockQuantity: z.number().min(0)
+// DTO's Schemas
+export const CreateProductSchema = ProductSchema.pick({
+  name: true,
+  price: true,
+  description: true,
+  categoryId: true,
+  isAvailable: true,
+  stockQuantity: true,
 });
 
-export const UpdateProductSchema = z.object({
-  name: z.string().min(2).max(100).optional(),
-  price: z.number().min(0).optional(),
-  description: z.string().max(250).optional(),
-  categoryId: z.string().optional(),
-  isAvailable: z.boolean().optional(),
-  stockQuantity: z.number().min(0).optional()
-});
+export const UpdateProductSchema = ProductSchema.partial();
 
+// Entity shape - Use this for service layer and business logic
+export type ProductEntity = z.infer<typeof ProductSchema>
+
+// Database shape - Only use for Mongoose schema definition and database operations
+export type ProductDocument = Omit<ProductEntity, "id">;
+
+// Input for controller usages
+export type CreateProductInput = z.input<typeof CreateProductSchema>
+export type UpdateProductInput = z.input<typeof UpdateProductSchema>
+
+// Infer/Output for service usages
 export type CreateProductDTO = z.infer<typeof CreateProductSchema>;
 export type UpdateProductDTO = z.infer<typeof UpdateProductSchema>;
+
+// Request types
+export type GetByIdParams = { id: string };
+export type GetBySlugParams = { slug: string };
