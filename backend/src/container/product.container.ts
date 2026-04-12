528bbaf73edc21e9de6446f8a10f4c06c3c7e6d9
Itsuki06 <trinhtung0604@gmail.com>
2026-04-13 00:37:23 +0700

    feat: Product container

--- backend/src/container/product.container.ts
+++ backend/src/container/product.container.ts
@@ -1,8 +1,9 @@
 import ProductController from "@/modules/products/product.controller";
 import ProductRepository from "@/modules/products/product.repository";
 import ProductService from "@/modules/products/product.service";
+import { categoryRepo } from "./category.container";
 
 const productRepo = new ProductRepository();
 
-export const productService = new ProductService(productRepo);
+export const productService = new ProductService(productRepo, categoryRepo);
 export const productController = new ProductController(productService);
