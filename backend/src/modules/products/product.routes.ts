d5fdc37e61faebd6c34d39d1d4b4fcd5ebd5d2a3
Itsuki06 <trinhtung0604@gmail.com>
2026-04-13 00:47:40 +0700

    feat: Update routes

--- backend/src/modules/products/product.routes.ts
+++ backend/src/modules/products/product.routes.ts
@@ -1,16 +1,32 @@
 import { productController } from "@/container/product.container";
+import { verifyAdmin, verifyAuth } from "@/middleware/auth.middleware";
+import { zodValidator } from "@/middleware/validate.middleware";
 import { Router } from "express";
+import { CreateProductSchema } from "./product.types";
+import { UpdateCategorySchema } from "../categories/category.types";
 
 const router = Router();
 
-router.post("/", productController.create);
-
+// GENERAL ROUTES
 router.get("/", productController.getAll);
-
+router.get("/:slug", productController.getOneBySlug);
 router.get("/category/:slug", productController.getListByCategory);
 
-router.get("/:id", productController.getOneById);
-router.put("/:id", productController.update);
-router.delete("/:id", productController.delete);
+// ADMIN ROUTES
+router.post(
+  "/",
+  verifyAuth,
+  verifyAdmin,
+  zodValidator(CreateProductSchema),
+  productController.create,
+);
+router.put(
+  "/:id",
+  verifyAuth,
+  verifyAdmin,
+  zodValidator(UpdateCategorySchema),
+  productController.update,
+);
+router.delete("/:id", verifyAuth, verifyAdmin, productController.delete);
 
 export default router;
