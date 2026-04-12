4dd4841b16d9c4c23f93fcfdb09327d1a64992ca
Itsuki06 <trinhtung0604@gmail.com>
2026-04-13 00:37:12 +0700

    feat: Category container

--- backend/src/container/category.container.ts
+++ backend/src/container/category.container.ts
@@ -2,7 +2,7 @@ import CategoryController from "@/modules/categories/category.controller";
 import CategoryRepository from "@/modules/categories/category.repository";
 import CategoryService from "@/modules/categories/category.service";
 
-const categoryRepo = new CategoryRepository();
+export const categoryRepo = new CategoryRepository();
 
 export const categoryService = new CategoryService(categoryRepo);
 export const categoryController = new CategoryController(categoryService);
