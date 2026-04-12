841eeaaa336ca574cf2956b83438b6748fa02c41
Itsuki06 <trinhtung0604@gmail.com>
2026-04-12 23:31:35 +0700

    feat: Product controller

--- backend/src/modules/products/product.controller.ts
+++ backend/src/modules/products/product.controller.ts
@@ -1,49 +1,63 @@
 import { NextFunction, Request, Response } from "express";
 import ProductService from "./product.service";
 import { ResponseHandler } from "../../utils/common/response.utils";
-
-const productService = new ProductService();
+import { PaginationParams, PaginationUtils } from "@/utils/common/pagination.utils";
+import { CreateProductInput } from "./product.types";
 
 export default class ProductController {
-  static async createProduct(req: Request, res: Response, next: NextFunction) {
+  constructor(private readonly productService: ProductService) {};
+  
+  create = async (req: Request<{}, any, CreateProductInput>, res: Response, next: NextFunction) => {
     try {
-      const product = await productService.createProduct(req.body);
-      return ResponseHandler.created(res, product, "Product created successfully.");
+      const newProduct = await this.productService.createProduct(req.body);
+      
+      return ResponseHandler.created(
+        res, 
+        newProduct, 
+        "Product created successfully."
+      );
     } catch (error) {
       next(error);
     }
   }
 
-  static async getAllProducts(_req: Request, res: Response, next: NextFunction) {
+  getAll = async (req: Request<{}, any, {}, PaginationParams>, res: Response, next: NextFunction) => {
     try {
-      const products = await productService.getAllProducts();
-      return ResponseHandler.success(res, products, "Products retrieved successfully.")
+      const { page, limit, offset } = PaginationUtils.extract(req.query);
+      
+      const products = await this.productService.getAllProducts(page, limit, offset);
+      
+      return ResponseHandler.success(
+        res, 
+        products, 
+        "Products retrieved successfully."
+      );
     } catch (error) {
       next(error);
     }
   }
 
-  static async getProductById(req: Request, res: Response, next: NextFunction) {
+  getOneById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const product = await productService.getProductById(String(req.params.productId));
+      const product = await this.productService.getProductById(String(req.params.productId));
       return ResponseHandler.success(res, product, "Product retrieved successfully.");
     } catch (error) {
       next(error);      
     }
   }
 
-  static async getProductsByCategory(req: Request, res: Response, next: NextFunction) {
+  getListByCategory = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const products = await productService.getProductsByCategory(String(req.params.categoryId));
+      const products = await this.productService.getProductsByCategory(String(req.params.categoryId));
       return ResponseHandler.success(res, products, "Products retrieved successfully.");
     } catch (error) {
       next(error);
     }
   }
 
-  static async updateProduct(req: Request, res: Response, next: NextFunction) {
+  update = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const product = await productService.updateProduct(
+      const product = await this.productService.updateProduct(
         String(req.params.productId),
         req.body,
       );
@@ -53,9 +67,9 @@ export default class ProductController {
     }
   }
 
-  static async deleteProduct(req: Request, res: Response, next: NextFunction) {
+  delete = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result = await productService.deleteProduct(String(req.params.productId));
+      const result = await this.productService.deleteProduct(String(req.params.productId));
       return ResponseHandler.success(res, result, "Product deleted successfully.");
     } catch (error) {
       next(error);
