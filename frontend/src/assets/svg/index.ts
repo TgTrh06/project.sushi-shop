299fa0f68523bb0dddd77c226581d3683cd010c1
Itsuki06 <shyorn0604@gmail.com>
2026-04-14 16:29:37 +0700

    feat: export images

---
+++ frontend/src/assets/svg/index.ts
@@ -0,0 +1,29 @@
+import searchIcon from "./search.svg";
+import menuIcon from "./menu.svg";
+import star from "./star.svg";
+import check from "./check.svg";
+import twitter from "./twitter.svg";
+import instagram from "./instagram.svg";
+import facebook from "./facebook.svg";
+import playCircle from "./play-circle.svg";
+import arrowUpRight from "./arrow-up-right.svg";
+import arrowLeft from "./arrow-left.svg";
+import arrowRight from "./arrow-right.svg";
+import arrowHorizontal from "./arrow-horizontal.svg";
+import arrowVertical from "./arrow-vertical.svg"; 
+
+export const Icon = {
+  search: searchIcon,
+  menu: menuIcon,
+  star: star,
+  check: check,
+  twitter: twitter,
+  instagram: instagram,
+  facebook: facebook,
+  playCircle: playCircle,
+  arrowLeft: arrowLeft,
+  arrowRight: arrowRight,
+  arrowVertical: arrowVertical,
+  arrowHorizontal: arrowHorizontal,
+  arrowUpRight: arrowUpRight,
+};
