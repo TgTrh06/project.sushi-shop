29c5dbadc68e147a1bf5138294ac773e9b5fa5e3
Itsuki06 <trinhtung0604@gmail.com>
2026-04-14 16:29:37 +0700

    feat: export images

---
+++ frontend/src/assets/image/index.ts
@@ -0,0 +1,56 @@
+// Backgrounds
+import aboutBg1 from "./src/about_bg1.png";
+import aboutBg2 from "./src/about_bg2.png";
+import popularBg from "./src/popular_bg.png";
+import subscribeBg from "./src/subscribe_bg.png";
+
+// Category / Special
+import japaneseDrinks from "./src/japanese_drinks.png";
+import japaneseSushi from "./src/japanese_sushi.png";
+import userAvatar from "./src/user.png";
+
+// Sushi List
+import sushiMain from "./src/sushi.png";
+import sushi1 from "./src/sushi-1.png";
+import sushi2 from "./src/sushi-2.png";
+import sushi3 from "./src/sushi-3.png";
+import sushi4 from "./src/sushi-4.png";
+import sushi5 from "./src/sushi-5.png";
+import sushi6 from "./src/sushi-6.png";
+import sushi7 from "./src/sushi-7.png";
+import sushi8 from "./src/sushi-8.png";
+import sushi9 from "./src/sushi-9.png";
+import sushi10 from "./src/sushi-10.png";
+import sushi11 from "./src/sushi-11.png";
+import sushi12 from "./src/sushi-12.png";
+
+export const Images = {
+  about: {
+    bg1: aboutBg1,
+    bg2: aboutBg2,
+  },
+  common: {
+    popularBg,
+    subscribeBg,
+    userAvatar,
+  },
+  categories: {
+    drinks: japaneseDrinks,
+    sushi: japaneseSushi,
+  },
+  sushi: {
+    main: sushiMain,
+    s1: sushi1,
+    s2: sushi2,
+    s3: sushi3,
+    s4: sushi4,
+    s5: sushi5,
+    s6: sushi6,
+    s7: sushi7,
+    s8: sushi8,
+    s9: sushi9,
+    s10: sushi10,
+    s11: sushi11,
+    s12: sushi12,
+  },
+};
