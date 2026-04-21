f280fb4377a564a3703894f7653281f1aa81fa49
Itsuki06 <trinhtung0604@gmail.com>
2026-04-21 23:25:25 +0700

    feat: Product detail pgae

---
+++ frontend/src/components/ui/ProductCard/index.tsx
@@ -0,0 +1,57 @@
+import "./ProductCard.css";
+import { Icon } from "@/assets/svg";
+import { Link } from "react-router-dom";
+import { WishListButton } from "@/components/ui/WishListButton";
+
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  rating: number;
+  category: string;
+}
+
+interface ProductCardProps {
+  product: Product;
+}
+
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <article className="product-card">
+      <Link to={`/product/${product.id}`} className="product-card__link">
+        <div className="product-card__image-wrapper">
+          <img
+            className="product-card__image"
+            src={product.image}
+            alt={product.name}
+            loading="lazy"
+          />
+        </div>
+
+        <div className="product-card__content">
+          <h4 className="product-card__title">{product.name}</h4>
+
+          <div className="product-card__details flex-between">
+            <div className="product-card__rating">
+              <img src={Icon.star} alt="star" />
+              <span>{product.rating}</span>
+            </div>
+
+            <p className="product-card__price">${product.price}.00</p>
+          </div>
+        </div>
+      </Link>
+
+      <div className="product-card__actions">
+        <Link to={`/product/${product.id}`} className="product-card__btn-link">
+          <button className="product-card__btn">
+            View Detail
+          </button>
+        </Link>
+        <WishListButton />
+      </div>
+    </article>
+  );
+};
