e9632edb150871d6664fb56265a63f1b0a2b1838
Itsuki06 <trinhtung0604@gmail.com>
2026-04-25 23:36:13 +0700

    feat: Review schema

---
+++ shared/src/schemas/review.schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+// =========================================================
+// BASE SCHEMAS FOR BOTH FRONTEND & BACKEND
+// =========================================================
+
+export const BaseReviewSchema = z.object({
+  id: z.string(),
+  productId: z.string(),
+  user: z.object({
+    id: z.string(),
+    name: z.string(),
+  }),
+  rating: z.number().min(1).max(5),
+  comment: z.string().min(1).max(1000),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export const CreateReviewSchema = BaseReviewSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
