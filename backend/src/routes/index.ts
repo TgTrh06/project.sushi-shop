c50931d92f2853dd80543920a2b7970e6565eeb1
Itsuki06 <trinhtung0604@gmail.com>
2026-04-25 23:35:47 +0700

    feat: Tiny change

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,6 +6,7 @@ import userRoutes from "@/modules/users/routes/user.routes";
 import categoryRoutes from "@/modules/categories/category.routes";
 import productRoutes from "@/modules/products/product.routes";
 import bookingRoutes from "@/modules/resevations/reservation.routes";
+import reviewRoutes from "@/modules/reviews/review.routes";
 import { UserModel } from "@/modules/users/user.model";
 import { ProductModel } from "@/modules/products/product.model";
 import { CategoryModel } from "@/modules/categories/category.model";
@@ -17,14 +18,6 @@ const router = Router();
 router.get("/", (req, res) => {
   res.send("Welcome to the Sushi Shop API!");
 });
-
-// API routes
-router.use("/auth", authRoutes);
-router.use("/admin", adminRoutes);
-router.use("/users", userRoutes);
-router.use("/categories", categoryRoutes);
-router.use("/products", productRoutes);
-router.use("/reservation", bookingRoutes);
 
 // Admin Stats endpoint
 router.get("/admin/stats", verifyAuth, verifyAdmin, async (_req, res, next) => {
@@ -52,5 +45,14 @@ router.get("/admin/stats", verifyAuth, verifyAdmin, async (_req, res, next) => {
     next(error);
   }
 });
+
+// API routes
+router.use("/auth", authRoutes);
+router.use("/admin", adminRoutes);
+router.use("/users", userRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/reservation", bookingRoutes);
+router.use("/reviews", reviewRoutes);
 
 export default router;
