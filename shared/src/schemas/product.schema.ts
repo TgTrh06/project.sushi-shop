ee72303b1c261759037d3d4d4a8c901ef7bd7295
Itsuki06 <shyorn0604@gmail.com>
2026-04-26 21:00:14 +0700

    feat: Product schema

--- shared/src/schemas/product.schema.ts
+++ shared/src/schemas/product.schema.ts
@@ -8,12 +8,29 @@ export const BaseProductSchema = z.object({
   id: z.string(),
   name: z.string().min(2).max(100),
   slug: z.string().min(2).max(100),
+  image: z.string().optional(),
   price: z.number().min(0),
-  imageUrl: z.url(),
+  gallery: z.array(z.string()).optional(),
   description: z.string().max(250).optional(),
-  categoryId: z.string(),
+  category: z.object({
+    id: z.string(),
+    name: z.string(),
+    slug: z.string(),
+  }),
   isAvailable: z.boolean().default(true).optional(),
-  stockQuantity: z.number().min(0),
+  stockQuantity: z.number().min(0).default(0),
+
+  ingredients: z.array(z.string()),
+
+  nutrition: z.array(z.object({
+    label: z.string(),
+    value: z.string()
+  })),
+
+  ratingSummary: z.object({
+    averageRating: z.number().default(0),
+    totalReviews: z.number().default(0)
+  }),
   createdAt: z.date(),
   updatedAt: z.date(),
 })
