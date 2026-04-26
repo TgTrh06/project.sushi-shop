94d8bd0731a646c88976c452e858b593b8aaa044
Itsuki06 <shyorn0604@gmail.com>
2026-04-26 21:02:01 +0700

    feat: Auth service

--- frontend/src/features/auth/auth.service.ts
+++ frontend/src/features/auth/auth.service.ts
@@ -4,21 +4,21 @@ import type { ApiResponse } from "@/types/response.type";
 import type { LoginFormInput, RegisterFormInput, ResetPasswordFormInput } from "@shared/schemas/auth.schema";
 
 export const authService = {
-  async signUp(input: RegisterFormInput): Promise<AuthResponse> {
+  async register(input: RegisterFormInput): Promise<AuthResponse> {
     const result = await api.post<ApiResponse<AuthResponse>>("/auth/register", { input });
     return result.data.data; // accessToken and user info are in the data property of the API response
   },
 
-  async signIn(input: LoginFormInput): Promise<AuthResponse> {
+  async login(input: LoginFormInput): Promise<AuthResponse> {
     const result = await api.post<ApiResponse<AuthResponse>>("/auth/login", { input });
     return result.data.data; // accessToken and user info are in the data property of the API response
   },
 
-  async signOut(): Promise<void> {
+  async logout(): Promise<void> {
     await api.post("/auth/logout");
   },
 
-  async refreshToken(): Promise<AuthResponse> {
+  async refresh(): Promise<AuthResponse> {
     const result = await api.post<ApiResponse<AuthResponse>>("/auth/refresh");
     return result.data.data; // accessToken
   },
