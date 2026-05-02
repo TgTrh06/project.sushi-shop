53a4baf433f2f4d013e0fd254104bc4bc7c2dff4
Itsuki06 <shyorn0604@gmail.com>
2026-05-02 22:33:28 +0700

    feat: Error middleware to print > 500 error only

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -8,17 +8,27 @@ export const globalErrorHandler = (
   res: Response,
   _next: NextFunction,
 ) => {
-  const statusCode = err.status || 500;
+  const statusCode = err.statusCode || err.status || 500;
 
-  // Log detail error into "path"
-  logger.error(err.message, {
-    metadata: {
-      stack: err.stack,
-      path: req.path,
-      method: req.method,
-      ip: req.ip,
-    }
-  });
+  // Log detail error based on status code
+  if (statusCode >= 500) {
+    logger.error(err.message, {
+      metadata: {
+        stack: err.stack,
+        path: req.path,
+        method: req.method,
+        ip: req.ip,
+      }
+    });
+  } else {
+    logger.warn(`[${statusCode}] ${err.message}`, {
+      metadata: {
+        path: req.path,
+        method: req.method,
+        ip: req.ip,
+      }
+    });
+  }
   
   // Setup response
   res.status(statusCode).json({
