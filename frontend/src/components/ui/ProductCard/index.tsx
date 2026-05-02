ddb38917780bd7d9c0d0dc5b5197038546a12cde
Itsuki06 <shyorn0604@gmail.com>
2026-05-02 23:07:28 +0700

    feat: Product card UI

--- frontend/src/components/ui/ProductCard/index.tsx
+++ frontend/src/components/ui/ProductCard/index.tsx
@@ -2,29 +2,20 @@ import "./ProductCard.css";
 import { Icon } from "@/assets/svg";
 import { Link } from "react-router-dom";
 import { WishListButton } from "@/components/ui/WishListButton";
-
-export interface Product {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  rating: number;
-  category: string;
-}
+import type { Product } from "@/features/products/product.types";
 
 interface ProductCardProps {
   product: Product;
 }
 
-
 export const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <article className="product-card">
-      <Link to={`/product/${product.id}`} className="product-card__link">
+      <Link to={`/product/${product.slug}`} className="product-card__link">
         <div className="product-card__image-wrapper">
           <img
             className="product-card__image"
-            src={product.image}
+            src={product.image || "https://placehold.co/600x400?text=Sushi"}
             alt={product.name}
             loading="lazy"
           />
@@ -36,7 +27,7 @@ export const ProductCard = ({ product }: ProductCardProps) => {
           <div className="product-card__details flex-between">
             <div className="product-card__rating">
               <img src={Icon.star} alt="star" />
-              <span>{product.rating}</span>
+              <span>{product.ratingSummary?.averageRating || 5.0}</span>
             </div>
 
             <p className="product-card__price">${product.price}.00</p>
@@ -45,7 +36,7 @@ export const ProductCard = ({ product }: ProductCardProps) => {
       </Link>
 
       <div className="product-card__actions">
-        <Link to={`/product/${product.id}`} className="product-card__btn-link">
+        <Link to={`/product/${product.slug}`} className="product-card__btn-link">
           <button className="product-card__btn">
             View Detail
           </button>
