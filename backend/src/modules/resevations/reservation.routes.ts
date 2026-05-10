c8e15bc49913d9fce95c2ffe09b40e8de8871182
Itsuki06 <shyorn0604@gmail.com>
2026-05-11 02:11:54 +0700

    feat: Get my reserv

--- backend/src/modules/resevations/reservation.routes.ts
+++ backend/src/modules/resevations/reservation.routes.ts
@@ -9,6 +9,9 @@ router.post("/", verifyAuth, reservationController.create); // Create reservatio
 router.get("/vnpay-return", reservationController.vnpayReturn); // VNPay callback
 router.get("/occupied-seats", reservationController.getOccupiedSeats); // Get occupied seats
 
+// USER ROUTES
+router.get("/my-reservations", verifyAuth, reservationController.getMyReservations); // Get user's reservations
+
 // ADMIN ROUTES
 router.get("/", verifyAuth, verifyAdmin, reservationController.getAll); // Get all reservations
 
